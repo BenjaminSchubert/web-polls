318d68f010ccd1f706382cab9757f7f8047e57b1
Basile Vu <basile.vu@gmail.com>
2016-12-08 18:56:44 +0100

    Adding some green

--- frontend/src/polls/poll/poll.service.ts
+++ frontend/src/polls/poll/poll.service.ts
@@ -17,17 +17,17 @@ export class PollService {
 
     private fetch() {
         this._$.next([
-            { active: true, owning: false, last_active: null, name: "Cours - 1", id: 1 },
-            { active: false, owning: false, last_active: "DATE", name: "Examen - 1", id: 2 },
-            { active: false, owning: false, last_active: "DATE", name: "Cours - 2", id: 3 },
-            { active: true, owning: false, last_active: "DATE", name: "Cours - 3", id: 4 },
-            { active: false, owning: true, last_active: "DATE", name: "Cours - 4", id: 5 },
-            { active: false, owning: true, last_active: "DATE", name: "Examen - 2", id: 6 },
-            { active: true, owning: false, last_active: "DATE", name: "Cours - 5", id: 7 },
-            { active: false, owning: false, last_active: "DATE", name: "Examen - 3", id: 8 },
-            { active: false, owning: false, last_active: "DATE", name: "Cours - 6", id: 9 },
-            { active: false, owning: false, last_active: "DATE", name: "Révisions", id: 10 },
-            { active: false, owning: false, last_active: "DATE", name: "Appréciations", id: 11 },
+            { active: true, owning: false, last_active: null, name: "Lesson - 1", id: 1 },
+            { active: false, owning: false, last_active: "DATE", name: "Exam - 1", id: 2 },
+            { active: false, owning: false, last_active: "DATE", name: "Lesson - 2", id: 3 },
+            { active: true, owning: false, last_active: "DATE", name: "Lesson - 3", id: 4 },
+            { active: false, owning: true, last_active: "DATE", name: "Lesson - 4", id: 5 },
+            { active: false, owning: true, last_active: "DATE", name: "Exam - 2", id: 6 },
+            { active: true, owning: false, last_active: "DATE", name: "Lesson - 5", id: 7 },
+            { active: false, owning: false, last_active: "DATE", name: "Exam - 3", id: 8 },
+            { active: false, owning: false, last_active: "DATE", name: "Lesson - 6", id: 9 },
+            { active: false, owning: false, last_active: "DATE", name: "Final exam", id: 10 },
+            { active: false, owning: false, last_active: "DATE", name: "Evaluations", id: 11 },
         ]);
     }
 
