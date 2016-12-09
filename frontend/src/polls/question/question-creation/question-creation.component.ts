9c130381e2940d167bcfa55d7826c946ddf7d944
Basile Vu <basile.vu@gmail.com>
2016-12-09 14:23:31 +0100

    Factoring css and moving submit button inside

--- frontend/src/polls/question/question-creation/question-creation.component.ts
+++ frontend/src/polls/question/question-creation/question-creation.component.ts
@@ -1,7 +1,11 @@
 import { Component } from "@angular/core";
 @Component({
     selector: "wp-question-creation",
-    styleUrls: ["question-creation.css", "../common.css"],
+    styleUrls: [
+        "question-creation.css",
+        "../question/question.css",
+        "../common.css"
+    ],
     templateUrl: "question-creation.html",
 })
 export class QuestionCreationComponent {}
