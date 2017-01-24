139c07373b53797b93a031471e6c2909f4259142
Benjamin Schubert <ben.c.schubert@gmail.com>
2017-01-24 19:17:05 +0100

    cleanup for radio button, breaking checkboxes

--- frontend/src/question/question-details.component.ts
+++ frontend/src/question/question-details.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup } from "@angular/forms";
 import { ErrorHandler } from "../base/error_handler";
 import { ActivatedRoute, Params, Router } from "@angular/router";
 import { IRoom } from "../room/stubs";
@@ -8,13 +7,14 @@ import { IQuestion } from "./stubs";
 import { IPoll } from "../poll/stubs";
 import { PollService } from "../poll/poll.service";
 import { RoomService } from "../room/room.service";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 
 @Component({
     templateUrl: "question-details.html",
 })
 export class QuestionComponent extends ErrorHandler implements OnInit {
-    public form: FormGroup = new FormGroup({});
+    public form: FormGroup;
     public poll: IPoll;
     public question: IQuestion;
     public room: IRoom;
@@ -23,24 +23,29 @@ export class QuestionComponent extends ErrorHandler implements OnInit {
     public questionIndex = 0;
     public editing: boolean = false;
 
-    constructor(
-        private route: ActivatedRoute,
-        public questions: QuestionService,
-        private polls: PollService,
-        private rooms: RoomService,
-        private router: Router,
-    ) {
+    constructor(private route: ActivatedRoute,
+                public questions: QuestionService,
+                private polls: PollService,
+                private rooms: RoomService,
+                private router: Router,
+                private builder: FormBuilder) {
         super();
     }
 
     public ngOnInit() {
         this.subscriptions.push(
             this.route.params.switchMap((p: Params) => this.questions.get(+p["question"]))
-                .subscribe((question: IQuestion) => this.question = question),
+                .subscribe((question: IQuestion) => {
+                    this.question = question;
+                    this.buildForm();
+                }),
             this.route.params.switchMap((p: Params) => this.polls.get(+p["poll"]))
                 .subscribe((poll: IPoll) => this.poll = poll),
             this.route.parent.parent.params.switchMap((p: Params) => this.rooms.get(+p["room"]))
-                .subscribe((room: IRoom) => this.room = room),
+                .subscribe((room: IRoom) => {
+                    this.room = room;
+                    this.disableIfOwner();
+                }),
             this.route.params.switchMap((p: Params) => this.questions.getForPoll(+p["poll"]))
                 .combineLatest(this.route.params.switchMap((p: Params) => this.questions.get(+p["question"])))
                 .subscribe((data: [IQuestion[], IQuestion]) => {
@@ -74,4 +79,21 @@ export class QuestionComponent extends ErrorHandler implements OnInit {
         this.router.navigate([this.room.id, this.poll.id, this.currentQuestions[this.questionIndex - 1].id]).then();
     }
 
+    public submit() {
+        console.log(this.form.value);
+    }
+
+    private buildForm() {
+        this.form = this.builder.group({
+            "answer": [null],
+        });
+    }
+
+    private disableIfOwner() {
+        if (this.room !== undefined && this.room.owning) {
+            this.form.disable();
+        } else {
+            this.form.enable();
+        }
+    }
 }
